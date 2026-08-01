059b63481627903e1ce7339ba7836111623f4ced
Welsonpeaches <a13805595521@163.com>
2026-08-01 19:54:16 +0800

    build: release 0.92.1

--- jianer/__init__.py
+++ jianer/__init__.py
@@ -27,7 +27,7 @@ from .plugins.runtime import (
     current_plugin_owner,
 )
 
-JIANER_BOT_VERSION = "0.92.0"
+JIANER_BOT_VERSION = "0.92.1"
 
 # listener = None
 
--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "aiohttp~=3.9.5",
         "arclet-alconna~=1.8",
         "requests~=2.31.0",
-        "httpx~=0.26.0",
+        "httpx>=0.28.1,<0.29",
         "lark-oapi~=1.6.8",
         "loguru~=0.7.3",
         "grpclib~=0.4.7",
